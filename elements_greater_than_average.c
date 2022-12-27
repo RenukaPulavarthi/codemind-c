28ceb2a85833a763b5a748b51826976e2183f5f2
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2022-12-27 11:46:00 +0530

    #include<stdio.h> int main() {     int n,i,s=0,av,c=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         s=s+a[i];     }     av=s/n;     for(i=0;i<n;i++)     {         if(a[i]>=av)         {             c=c+1;         }     }     printf("%d",c); }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,s=0,av,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=s+a[i];
+    }
+    av=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=av)
+        {
+            c=c+1;
+        }
+    }
+    printf("%d",c);
+}
