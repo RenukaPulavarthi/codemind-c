1f90231cffaf16ad7dda96700f453fc1d707caf3
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2023-10-22 15:41:38 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     int s=0;     int m=a<b?(a<c?a:c):(b<c?b:c);     if(a!=m)   s+=a;     if(b!=m)   s+=b;     if(c!=m)    s+=c;     printf("%d",s); }

--- Hermoine_and_Spells.c
+++ Hermoine_and_Spells.c
@@ -1,18 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,min,t;
+    int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
-    if(a<b&&a<c)
-    {
-        min=a;
-    }
-    else if(b<a&&b<c)
-    {
-        min=b;
-    }
-    else
-      min=c;
-    t=a+b+c-min;
-    printf("%d",t);
+    int s=0;
+    int m=a<b?(a<c?a:c):(b<c?b:c);
+    if(a!=m)   s+=a;
+    if(b!=m)   s+=b;
+    if(c!=m)    s+=c;
+    printf("%d",s);
 }
